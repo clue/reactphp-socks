5a51c8cb3497f15155923c20b120859aebfaa0be
Christian Lück <christian@lueck.tv>
2012-09-11 00:26:53 +0200

    Fix warning when connecting to hostnames

--- Socks5.class.php
+++ Socks5.class.php
@@ -61,23 +61,23 @@ class Socks5 extends Socks4{
         if($data['version'] !== 0x05){
             throw new Exception('Version/Protocol mismatch');
         }
-        if($data['method'] === 0x02 && $this->auth !== NULL){                   // username/password authentication
+        if($data['method'] === 0x02 && $this->auth !== NULL){                  // username/password authentication
             $response = $this->streamWriteRead($stream,$this->auth,2);
             $data = unpack('Cversion/Cstatus',$response);
             
             if($data['version'] !== 0x01 || $data['status'] !== 0x00){
                 throw new Exception('Username/Password authentication failed');
             }
-        }else if($data['method'] !== 0x00){                                     // any other method than "no authentication"
+        }else if($data['method'] !== 0x00){                                    // any other method than "no authentication"
             throw new Exception('Unacceptable authentication method requested');
         }
         
-        $ip = inet_pton($hostname);                                             // do not resolve hostname. only try to convert to (binary/packed) IP
+        $ip = @inet_pton($hostname);                                            // do not resolve hostname. only try to convert to (binary/packed) IP
         
         $packet = pack('C3',0x05,$method,0x00);
-        if($ip === false){                                                      // not an IP, send as hostname
+        if($ip === false){                                                     // not an IP, send as hostname
             $packet .= pack('C2',0x03,strlen($hostname)).$hostname;
-        }else{                                                                  // send as IPv4 / IPv6
+        }else{                                                                 // send as IPv4 / IPv6
             $packet .= pack('C',(strpos($hostname,':') === false) ? 0x01 : 0x04).$ip;
         }
         $packet .= pack('n',$port);
@@ -90,11 +90,11 @@ class Socks5 extends Socks4{
         }
         if($data['type'] === 0x01){                                             // ipv4 address
             $this->streamRead($stream,6);                                       // skip IP and port
-        }else if($data['type'] === 0x03){                                       // domain name
+        }else if($data['type'] === 0x03){                                      // domain name
             $response = $this->streamRead($stream,1);                           // read domain name length
             $data = unpack('Clength',$response);
             $this->streamRead($stream,$data['length']+2);                       // skip domain name and port
-        }else if($data['type'] === 0x04){                                       // IPv6 address
+        }else if($data['type'] === 0x04){                                      // IPv6 address
             $this->streamRead($stream,18);                                      // skip IP and port
         }else{
             throw new Exception('Invalid SOCKS reponse: Invalid address type');
