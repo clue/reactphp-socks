b8c56e42cd7add590acb3592a6e0208521ca6bd6
Christian Lück <christian@lueck.tv>
2012-11-28 20:13:13 +0100

    Additional connection attempts and HTTP request

--- example.php
+++ example.php
@@ -1,27 +1,77 @@
 <?php
 
+use React\HttpClient\Client as HttpClient;
+use React\HttpClient\Response;
+use React\Stream\Stream;
+
 include_once __DIR__.'/vendor/autoload.php';
 include_once __DIR__.'/Factory.php';
 include_once __DIR__.'/Client.php';
+// include_once __DIR__.'/ConnectionManagerTimeoutInterface.php';
+// include_once __DIR__.'/ConnectionManagerTimeout.php';
+include_once __DIR__.'/ConnectionManagerFsockopen.php';
 
-$loop = $loop = React\EventLoop\Factory::create(); 
+$loop = $loop = React\EventLoop\Factory::create();
 
-$factory = new React\Dns\Resolver\Factory();
-$dns = $factory->create('8.8.8.8', $loop);
+$dnsResolverFactory = new React\Dns\Resolver\Factory();
+$dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
 
 $factory = new Factory($loop, $dns);
 
-$client = $factory->createClient('localhost', 9050);
+$client = $factory->createClient('127.0.0.1', 9050);
 
-$client->getConnection(function($stream, $error){
-    echo 'connected to target';
-}, 'www.google.com', 80);
+function ex(Exception $exception=null){
+    if($exception !== null){
+        echo 'message: '.$exception->getMessage().PHP_EOL;
+        while(($exception = $exception->getPrevious())){
+            echo 'previous: '.$exception->getMessage().PHP_EOL;
+        }
+    }
+}
+
+$client->getConnection('www.google.com', 80)->then(
+    function ($stream) {
+        echo 'connection OK'.PHP_EOL;
+    },
+    function (Exception $error) {
+        echo 'connection ';
+        ex($error);
+    }
+);
+
+$client->getConnection('www.google.commm', 80)->then(
+    null,
+    function (Exception $error) {
+        echo 'www.google.commm ';
+        ex($error);
+    }
+);
+
+$client->getConnection('www.google.com', 8080)->then(
+    null,
+    function (Exception $error) {
+        echo 'www.google.com:8080 ';
+        ex($error);
+    }
+);
 
 
-$loop->addTimer(10, function(){
-    echo 'timeout?';
+// $factory = new React\HttpClient\Factory();
+// $httpclient = $factory->create($loop, $dns);
+$httpclient = $client->createHttpClient();
+
+$request = $httpclient->request('GET','http://www.google.com/',array('user-agent'=>'none'));
+$request->on('response',function(Response $response){
+    echo '[response1]'.PHP_EOL;
+    $response->on('data',function($data){
+        echo $data;
+    });
+});
+$request->end();
+
+$loop->addTimer(8, function() use ($loop){
+    $loop->stop();
+    echo 'STOP - stopping mainloop after 5 seconds'.PHP_EOL;
 });
 
 $loop->run();
-
-echo 'done?';
