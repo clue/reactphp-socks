d6aa8f8155a4e7106ad66af474c1c335c00c9571
Christian Lück <christian@lueck.tv>
2012-11-28 21:26:03 +0100

    Unify code style

--- example.php
+++ example.php
@@ -7,8 +7,6 @@ use React\Stream\Stream;
 include_once __DIR__.'/vendor/autoload.php';
 include_once __DIR__.'/Factory.php';
 include_once __DIR__.'/Client.php';
-// include_once __DIR__.'/ConnectionManagerTimeoutInterface.php';
-// include_once __DIR__.'/ConnectionManagerTimeout.php';
 include_once __DIR__.'/ConnectionManagerFsockopen.php';
 
 $loop = $loop = React\EventLoop\Factory::create();
@@ -20,18 +18,19 @@ $factory = new Factory($loop, $dns);
 
 $client = $factory->createClient('127.0.0.1', 9050);
 
-function ex(Exception $exception=null){
-    if($exception !== null){
-        echo 'message: '.$exception->getMessage().PHP_EOL;
-        while(($exception = $exception->getPrevious())){
-            echo 'previous: '.$exception->getMessage().PHP_EOL;
+function ex(Exception $exception=null)
+{
+    if ($exception !== null) {
+        echo 'message: ' . $exception->getMessage() . PHP_EOL;
+        while (($exception = $exception->getPrevious())) {
+            echo 'previous: ' . $exception->getMessage() . PHP_EOL;
         }
     }
 }
 
 $client->getConnection('www.google.com', 80)->then(
     function ($stream) {
-        echo 'connection OK'.PHP_EOL;
+        echo 'connection OK' . PHP_EOL;
     },
     function (Exception $error) {
         echo 'connection ';
@@ -60,18 +59,19 @@ $client->getConnection('www.google.com', 8080)->then(
 // $httpclient = $factory->create($loop, $dns);
 $httpclient = $client->createHttpClient();
 
-$request = $httpclient->request('GET','http://www.google.com/',array('user-agent'=>'none'));
-$request->on('response',function(Response $response){
-    echo '[response1]'.PHP_EOL;
-    $response->on('data',function($data){
+$request = $httpclient->request('GET', 'http://www.google.com/', array('user-agent'=>'none'));
+$request->on('response', function (Response $response) {
+    echo '[response1]' . PHP_EOL;
+    var_dump($response->getHeaders());
+    $response->on('data', function ($data) {
         echo $data;
     });
 });
 $request->end();
 
-$loop->addTimer(8, function() use ($loop){
+$loop->addTimer(8, function() use ($loop) {
     $loop->stop();
-    echo 'STOP - stopping mainloop after 5 seconds'.PHP_EOL;
+    echo 'STOP - stopping mainloop after 5 seconds' . PHP_EOL;
 });
 
 $loop->run();
