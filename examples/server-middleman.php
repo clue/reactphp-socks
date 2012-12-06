ae45256833c2556c8b6f9cf03db2fe3779fde149
Christian Lück <christian@lueck.tv>
2012-12-07 00:55:58 +0100

    Add middleman example

---
+++ examples/server-middleman.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once __DIR__.'/../vendor/autoload.php';
+
+$loop = React\EventLoop\Factory::create();
+
+$dnsResolverFactory = new React\Dns\Resolver\Factory();
+$dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
+
+$factory = new Socks\Factory($loop, $dns);
+
+// set next SOCKS server as target
+$target = $factory->createClient('127.0.0.1',9050);
+
+// start a new server which forwards all connections to another SOCKS server
+$server = new Socks\Server($loop, $target);
+
+$server->listen('9051','localhost');
+
+$loop->run();
