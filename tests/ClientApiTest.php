8de543ac8d72f73511ce4912afd11b189304ebbb
Christian Lück <christian@lueck.tv>
2013-05-22 21:21:05 +0200

    Add unit test bootstrap skeleton

--- tests/ClientApiTest.php
+++ tests/ClientApiTest.php
@@ -1,8 +1,7 @@
 <?php
 
-(include_once __DIR__.'/../vendor/autoload.php') OR die(PHP_EOL.'ERROR: composer autoloader not found, run "composer install" or see README for instructions'.PHP_EOL);
-
-class ClientApiTest extends PHPUnit_Framework_TestCase{
+class ClientApiTest extends TestCase
+{
     public function setUp(){
         $loop = React\EventLoop\Factory::create();
 
