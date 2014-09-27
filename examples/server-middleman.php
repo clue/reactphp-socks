0c44cf20c828a023dfd8b89c8017d7ed32388d66
Christian Lück <christian@lueck.tv>
2014-09-27 15:30:58 +0200

    Clean up examples by moving assertions to functional test suite

--- examples/server-middleman.php
+++ examples/server-middleman.php
@@ -2,18 +2,21 @@
 
 use Clue\React\Socks\Client;
 use Clue\React\Socks\Server;
+use React\Socket\Server as Socket;
+
 include_once __DIR__.'/../vendor/autoload.php';
 
 $loop = React\EventLoop\Factory::create();
 
-// set next SOCKS server as target
+// set next SOCKS server localhost:9050 as target
 $target = new Client($loop, '127.0.0.1',9050);
 $target->setAuth('user','p@ssw0rd');
 
-// start a new server which forwards all connections to another SOCKS server
-$socket = new React\Socket\Server($loop);
+// listen on localhost:9051
+$socket = new Socket($loop);
 $socket->listen(9051, 'localhost');
 
+// start a new server which forwards all connections to the other SOCKS server
 $server = new Server($loop, $socket, $target->createConnector());
 
 echo 'SOCKS server listening on localhost:9051 (which forwards everything to SOCKS server 127.0.0.1:9050)' . PHP_EOL;
--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -3,42 +3,73 @@
 use React\Stream\Stream;
 use Clue\React\Socks\Client;
 use Clue\React\Socks\Server;
+use React\Promise\PromiseInterface;
 
 class FunctionalTest extends TestCase
 {
     private $loop;
+    private $client;
+    private $server;
 
     public function setUp()
     {
         $this->loop = React\EventLoop\Factory::create();
-    }
 
-    public function testClientConnection()
-    {
         $socket = $this->createSocketServer();
         $port = $socket->getPort();
         $this->assertNotEquals(0, $port);
 
-        $server = new Server($this->loop, $socket);
+        $this->server = new Server($this->loop, $socket);
+        $this->client = new Client($this->loop, '127.0.0.1', $port);
+    }
 
-        $server->on('connection', function () use ($socket) {
-            // close server socket once first connection has been established
-            $socket->shutdown();
-        });
+    public function testConnection()
+    {
+        $this->assertResolveStream($this->client->getConnection('www.google.com', 80));
+    }
 
-        $client = new Client($this->loop, '127.0.0.1', $port);
+    public function testConnectorOkay()
+    {
+        $tcp = $this->client->createConnector();
 
-        $that = $this;
-        $client->getConnection('www.google.com', 80)->then(
-            function (Stream $stream) {
-                $stream->close();
-            },
-            function ($error) use ($that) {
-                $that->fail('Unable to connect');
-            }
-        );
+        $this->assertResolveStream($tcp->create('www.google.com', 80));
+    }
 
-        $this->loop->run();
+    public function testConnectorInvalidDomain()
+    {
+        $tcp = $this->client->createConnector();
+
+        $this->assertRejectPromise($tcp->create('www.google.commm', 80));
+    }
+
+    public function testConnectorInvalidUnboundPortTimeout()
+    {
+        $this->client->setTimeout(0.1);
+        $tcp = $this->client->createConnector();
+
+        $this->assertRejectPromise($tcp->create('www.google.com', 8080));
+    }
+
+    public function testSecureConnectorOkay()
+    {
+        $ssl = $this->client->createSecureConnector();
+
+        $this->assertResolveStream($ssl->create('www.google.com', 443));
+    }
+
+    public function testSecureConnectorInvalidPlaintextIsNotSsl()
+    {
+        $ssl = $this->client->createSecureConnector();
+
+        $this->assertRejectPromise($ssl->create('www.google.com', 80));
+    }
+
+    public function testSecureConnectorInvalidUnboundPortTimeout()
+    {
+        $this->client->setTimeout(0.1);
+        $ssl = $this->client->createSecureConnector();
+
+        $this->assertRejectPromise($ssl->create('www.google.com', 8080));
     }
 
     private function createSocketServer()
@@ -47,5 +78,46 @@ class FunctionalTest extends TestCase
         $socket->listen(0);
 
         return $socket;
+    }
+
+    private function assertResolveStream($promise)
+    {
+        $this->expectPromiseResolve($promise);
+
+        $promise->then(function ($stream) {
+            $stream->close();
+        });
+
+        $this->waitFor($promise);
+    }
+
+    private function assertRejectPromise($promise)
+    {
+        $this->expectPromiseReject($promise);
+
+        $this->setExpectedException('Exception');
+        $this->waitFor($promise);
+    }
+
+    private function waitFor(PromiseInterface $promise)
+    {
+        $resolved = null;
+        $exception = null;
+
+        $promise->then(function ($c) use (&$resolved) {
+            $resolved = $c;
+        }, function($error) use (&$exception) {
+            $exception = $error;
+        });
+
+        while ($resolved === null && $exception === null) {
+            $this->loop->tick();
+        }
+
+        if ($exception !== null) {
+            throw $exception;
+        }
+
+        return $resolved;
     }
 }
