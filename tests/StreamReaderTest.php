54559a64b26f7ee42889f465e36bef0bf4b67712
Christian Lück <christian@lueck.tv>
2014-09-27 12:56:15 +0200

    Add tests for StreamReader

--- tests/StreamReaderTest.php
+++ tests/StreamReaderTest.php
@@ -11,10 +11,49 @@ class StreamReaderTest extends TestCase
         $this->reader = new StreamReader();
     }
 
-    public function testA()
+    public function testReadByteAssertCorrect()
     {
-        $that = $this;
+        $this->reader->readByteAssert(0x01)->then($this->expectCallableOnce(0x01));
 
+        $this->reader->write("\x01");
+    }
+
+    public function testReadByteAssertInvalid()
+    {
+        $this->reader->readByteAssert(0x02)->then(null, $this->expectCallableOnce());
+
+        $this->reader->write("\x03");
+    }
+
+    public function testReadStringNull()
+    {
+        $this->reader->readStringNull()->then($this->expectCallableOnce('hello'));
+
+        $this->reader->write("hello\x00");
+    }
+
+    public function testReadStringLength()
+    {
+        $this->reader->readLength(5)->then($this->expectCallableOnce('hello'));
+
+        $this->reader->write('he');
+        $this->reader->write('ll');
+        $this->reader->write('o ');
+
+        $this->assertEquals(' ', $this->reader->getBuffer());
+    }
+
+    public function testReadBuffered()
+    {
+        $this->reader->write('hello');
+
+        $this->reader->readLength(5)->then($this->expectCallableOnce('hello'));
+
+        $this->assertEquals('', $this->reader->getBuffer());
+    }
+
+    public function testSequence()
+    {
         $this->reader->readByte()->then($this->expectCallableOnce(ord('h')));
         $this->reader->readByteAssert(ord('e'))->then($this->expectCallableOnce(ord('e')));
         $this->reader->readLength(4)->then($this->expectCallableOnce('llo '));
@@ -23,5 +62,21 @@ class StreamReaderTest extends TestCase
         $this->reader->write('hello world');
 
         $this->assertEquals('rld', $this->reader->getBuffer());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInvalidStructure()
+    {
+        $this->reader->readBinary(array('invalid' => 'y'));
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInvalidCallback()
+    {
+        $this->reader->readBufferCallback(array());
     }
 }
