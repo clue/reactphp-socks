91307c3eada2cbe476695ba609ff5332f954c848
Christian Lück <christian@lueck.tv>
2016-11-20 15:16:26 +0100

    Use clue/block-react in order to simplify tests and add timeout

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -3,7 +3,7 @@
 use React\Stream\Stream;
 use Clue\React\Socks\Client;
 use Clue\React\Socks\Server\Server;
-use React\Promise\PromiseInterface;
+use Clue\React\Block;
 
 class FunctionalTest extends TestCase
 {
@@ -189,7 +189,7 @@ class FunctionalTest extends TestCase
             $stream->close();
         });
 
-        $this->waitFor($promise);
+        Block\await($promise, $this->loop, 2.0);
     }
 
     private function assertRejectPromise($promise)
@@ -197,28 +197,7 @@ class FunctionalTest extends TestCase
         $this->expectPromiseReject($promise);
 
         $this->setExpectedException('Exception');
-        $this->waitFor($promise);
-    }
 
-    private function waitFor(PromiseInterface $promise)
-    {
-        $resolved = null;
-        $exception = null;
-
-        $promise->then(function ($c) use (&$resolved) {
-            $resolved = $c;
-        }, function($error) use (&$exception) {
-            $exception = $error;
-        });
-
-        while ($resolved === null && $exception === null) {
-            $this->loop->tick();
-        }
-
-        if ($exception !== null) {
-            throw $exception;
-        }
-
-        return $resolved;
+        Block\await($promise, $this->loop, 2.0);
     }
 }
