4083e104320f810245d25f6669ec06fe8b1f5a49
Christian Lück <christian@lueck.tv>
2017-04-21 19:58:40 +0200

    Proxy chaining examples now accept a variable number of next hops

--- examples/03-proxy-chaining.php
+++ examples/03-proxy-chaining.php
@@ -6,24 +6,34 @@ use React\Socket\ConnectionInterface;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$first = isset($argv[1]) ? $argv[1] : '127.0.0.1:1080';
-$second = isset($argv[2]) ? $argv[2] : $first;
+if (!isset($argv[1])) {
+    echo 'No arguments given! Run with <proxy1> [<proxyN>...]' . PHP_EOL;
+    echo 'You can add 1..n proxies in the path' . PHP_EOL;
+    exit(1);
+}
+
+$path = array_slice($argv, 1);
+
+// Alternatively, you can also hard-code this value like this:
+//$path = array('127.0.0.1:9051', '127.0.0.1:9052', '127.0.0.1:9053');
 
 $loop = React\EventLoop\Factory::create();
 
-// https via the proxy chain  "foo -> bar -> target"
-// please note how the client uses bar (not foo!), which in turn then uses foo
-// this creates a TCP/IP connection to foo, which then connects to bar, which then connects to the target
-$foo = new Client($first, new Connector($loop));
-$bar = new Client($second, $foo);
+// set next SOCKS server chain via p1 -> p2 -> p3 -> destination
+$connector = new Connector($loop);
+foreach ($path as $proxy) {
+    $connector = new Client($proxy, $connector);
+}
 
+// please note how the client uses p3 (not p1!), which in turn then uses the complete chain
+// this creates a TCP/IP connection to p1, which then connects to p2, then to p3, which then connects to the target
 $connector = new Connector($loop, array(
-    'tcp' => $bar,
+    'tcp' => $connector,
     'timeout' => 3.0,
     'dns' => false
 ));
 
-echo 'Demo SOCKS client connecting to SOCKS proxy server chain ' . $first . ' and ' . $second . PHP_EOL;
+echo 'Demo SOCKS client connecting to SOCKS proxy server chain ' . implode(' -> ', $path) . PHP_EOL;
 
 $connector->connect('tls://www.google.com:443')->then(function (ConnectionInterface $stream) {
     echo 'connected' . PHP_EOL;
--- examples/21-server-proxy-chaining.php
+++ examples/21-server-proxy-chaining.php
@@ -1,6 +1,6 @@
 <?php
 
-// A SOCKS server that forwards (proxy chaining) to a another SOCKS server
+// A SOCKS server that forwards (proxy chaining) to other SOCKS servers
 
 use Clue\React\Socks\Client;
 use Clue\React\Socks\Server;
@@ -9,20 +9,34 @@ use React\Socket\Connector;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+if (!isset($argv[2])) {
+    echo 'No arguments given! Run with <listen> <proxy1> [<proxyN>...]' . PHP_EOL;
+    echo 'You can add 1..n proxies in the path' . PHP_EOL;
+    exit(1);
+}
+
+$listen = $argv[1];
+$path = array_slice($argv, 2);
+
+// Alternatively, you can also hard-code these values like this:
+//$listen = '127.0.0.1:9050';
+//$path = array('127.0.0.1:9051', '127.0.0.1:9052', '127.0.0.1:9053');
+
 $loop = React\EventLoop\Factory::create();
 
-// set next SOCKS server 127.0.0.1:1080 as target
+// set next SOCKS server chain -> p1 -> p2 -> p3 -> destination
 $connector = new Connector($loop);
-$proxy = isset($argv[2]) ? $argv[2] : 'user:p%40ssw0rd@127.0.0.1:1080';
-$target = new Client($proxy, $connector);
+foreach ($path as $proxy) {
+    $connector = new Client($proxy, $connector);
+}
 
 // listen on 127.0.0.1:1080 or first argument
-$listen = isset($argv[1]) ? $argv[1] : '127.0.0.1:1080';
 $socket = new Socket($listen, $loop);
 
 // start a new server which forwards all connections to the other SOCKS server
-$server = new Server($loop, $socket, $target);
+$server = new Server($loop, $socket, $connector);
 
-echo 'SOCKS server listening on ' . $socket->getAddress() . ' (which forwards everything to target SOCKS server ' . $proxy . ')' . PHP_EOL;
+echo 'SOCKS server listening on ' . $socket->getAddress() . PHP_EOL;
+echo 'Forwarding via: ' . implode(' -> ', $path) . PHP_EOL;
 
 $loop->run();
